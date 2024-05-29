2dbcb51709ac28ac2c43f8f5743d79e2b66068f9
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-05-29 20:32:42 +0530

    Basic Express server

---
+++ 1.BasicNode/1.Intro/2.HTTP/index.js
@@ -0,0 +1,35 @@
+// HTTP METHODS:
+
+/*
+
+// READ
+1. GET: getting the resource from the server (Searching sachin on google.com) (GETS THE RESOURCE).
+
+// CREATE
+2. POST: If you want to send some data to the server to create a RESOURCE. (CREATING RESOURCE)
+
+// UPDATE
+3. PUT: 3.1 if the resource we want to EDIT is PRESENT then it will replace the WHOLE RESOURCE
+        3.2 if the resource we want to EDIT is NOT PRESENT then it will CREATE THE NEW RESOURCE
+
+4. PATCH: Patch is also used for EDITING the resource but PARTIALLY.
+
+// DELETE
+5. DELETE: TO DELETE A RESOURCE
+
+6. OPTIONS/PREFLIGHT: its a request that helps us to know if the SERVER is ready or not.
+
+
+TODO: add resource LINK.
+
+READ: ABOUT Idempotent
+
+
+*/
+
+// HTTP STATUS CODES:
+
+// 2XX: (201, 200 ): SUCCESSFUL RESPONSE
+// 3XX: Redirection
+// 4XX: (404 not found) Client Error
+// 5XX: (500 internal server error) SERVER ERROR
---
+++ 2.ExpressServerIntro/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+const server = express();
+
+// MIDDLEWARE NEEDED ...
+/*
+Middleware that will PARSE EVERY REQUEST and then forward the request to 
+ route handlers
+ */
+
+//  I have not given any url to server.use -> so that I can handle for any ROUTE
+server.use(express.json());
+
+// HOME ROUTE
+
+// use -> GET, PUT, POST, DELETE, .....
+/*
+server.use("/", (req, res) => {
+  res.end("WELCOME TO HOME PAGE EXPRESS APP");
+});
+*/
+
+server.get("/", (req, res) => {
+  res.end("WELCOME TO THE HOME PAGE");
+});
+
+// --------- HANDLE QUERY PARAMS -----------
+
+server.get("/fitness", (req, res) => {
+  const queryParams = req.query;
+  console.log(queryParams);
+
+  const { type } = queryParams;
+
+  if (type === "gym") {
+    res.end("fitness WELCOME TO THE GYM AND EQUIPMENT ");
+  } else {
+    res.end("fitness :::: " + queryParams.type);
+  }
+});
+
+// ------------ HANDLE PARAMS -------------
+
+server.get("/sport/:id", (req, res) => {
+  const id = req.params["id"];
+
+  res.end("PRODUCT ::: " + id);
+});
+
+// ----------- HANDLE THE BODY -----------
+
+server.post("/register/", (req, res) => {
+  const body = req.body;
+
+  const { name } = body;
+
+  res.end("REGISTERED SUCCESSFULLY ::: WELCOME " + name);
+});
+
+server.listen(8081, () => {
+  console.log("Port working on 8081");
+});
